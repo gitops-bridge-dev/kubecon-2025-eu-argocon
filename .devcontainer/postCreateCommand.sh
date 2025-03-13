42db71a6c395200c19f48ef86a95b2c50eb997af
Carlos Santana <carrlos@amazon.com>
2025-03-12 23:15:56 -0400

    add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# For Kubectl AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# For Kubectl ARM64
+[ $(uname -m) = aarch64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# For Kind AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -sLo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
+# For Kind ARM64
+[ $(uname -m) = aarch64 ] && curl -sLo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# setup autocomplete for kubectl and alias k
+sudo apt-get update -y && sudo apt-get install bash-completion -y
+mkdir $HOME/.kube
+echo "source <(kubectl completion bash)" >> $HOME/.bashrc
+echo "alias k=kubectl" >> $HOME/.bashrc
+echo "complete -F __start_kubectl k" >> $HOME/.bashrc
+# if docker network kind is not exist, create it
+if [ -z "$(docker network ls | grep kind)" ]; then
+docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.driver.mtu=1500 --subnet fc00:f853:ccd:e793::/64 kind
+fi
